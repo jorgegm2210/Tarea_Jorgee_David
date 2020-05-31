625d6ef07d4395ef930c5604e1311a9fd02128fe
jorgremar99 <jorgegm@alumnos.iesgalileo.es>
2020-05-31 12:24:57 +0200

    no message

--- practicaGit/src/practicaGit/Year.java
+++ practicaGit/src/practicaGit/Year.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 package practicaGit;
 
@@ -27,41 +27,3 @@ return indice;
 return -1;
 }
 }
-
-    
-    
-
-=======
-
-package practicaGit;
-
-import java.util.Arrays;
-public class Year {
-private static String[] meses={"enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"};
-private static int[] dias={ 31,28,31,30,31,30,31,31,30,31,30,31};
-public static int getDiasMes(String nombre){
-int indice=buscar(nombre);
-return dias[indice];
-}
-public static int getDiasMes(int nMes){
-int indice=nMes-1;
-return dias[indice];
-}
-public static String getNombreMes(int nMes){
-int indice=nMes-1;
-return meses[indice];
-}
-private static int buscar(String mesBuscado){
-for(int indice=0;indice<12;indice++){
-if(mesBuscado.equals(meses[indice])){
-return indice;
-}
-}
-return -1;
-}
-}
-
-    
-    
-
->>>>>>> dc90cd1afbc9babdaf977a9b5b36ed0fab878587
